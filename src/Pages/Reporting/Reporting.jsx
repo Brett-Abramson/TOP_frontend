bdb45339e7f529b5c1195489b30f0c9f53106ee2
Brett Abramson <brett.m.abramson@gmail.com>
2023-11-27 13:44:31 -0800

    updated report.jsx to search queryparams, still troubleshooting reporting flow

--- src/Pages/Reporting/Reporting.jsx
+++ src/Pages/Reporting/Reporting.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import ReqQuestion1 from "../../Components/Reporting/CriticalQuestions/ReqQuestion1/ReqQuestion1";
 import ReqQuestion2 from "../../Components/Reporting/CriticalQuestions/ReqQuestion2/ReqQuestion2";
 import ReqQuestion3 from "../../Components/Reporting/CriticalQuestions/ReqQuestion3/ReqQuestion3";
@@ -13,6 +14,15 @@ import Home from "../../Components/Reporting/LandingPage/Home";
 
 export default function Reporting() {
   const [demoPage, setDemoPage] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const page = queryParams.get("demoPage");
+    if (page) {
+      setDemoPage(parseInt(page, 10));
+    }
+  }, [location]);
 
   useEffect(() => {
     const savedDemoPage = localStorage.getItem("demoPage");
@@ -27,29 +37,54 @@ export default function Reporting() {
     window.scrollTo(0, targetY);
   }, [demoPage]);
 
-
-
   return (
     <div>
-      {demoPage === 0 ? <Home demoPage={demoPage} setDemoPage={setDemoPage} /> : ""}
+      {demoPage === 0 ? (
+        <Home demoPage={demoPage} setDemoPage={setDemoPage} />
+      ) : (
+        ""
+      )}
 
       {/* -------------Required Questions-------------- */}
 
-      {demoPage === 1 ? <ReqQuestion1 demoPage={demoPage} setDemoPage={setDemoPage} /> : ""}
+      {demoPage === 1 ? (
+        <ReqQuestion1 demoPage={demoPage} setDemoPage={setDemoPage} />
+      ) : (
+        ""
+      )}
 
-      {demoPage === 2 ? <ReqQuestion2 demoPage={demoPage} setDemoPage={setDemoPage} /> : ""}
+      {demoPage === 2 ? (
+        <ReqQuestion2 demoPage={demoPage} setDemoPage={setDemoPage} />
+      ) : (
+        ""
+      )}
 
-      {demoPage === 3 ? <ReqQuestion3 demoPage={demoPage} setDemoPage={setDemoPage} /> : ""}
+      {demoPage === 3 ? (
+        <ReqQuestion3 demoPage={demoPage} setDemoPage={setDemoPage} />
+      ) : (
+        ""
+      )}
 
-      {demoPage === 4 ? <ReqQuestion4 demoPage={demoPage} setDemoPage={setDemoPage} /> : ""}
+      {demoPage === 4 ? (
+        <ReqQuestion4 demoPage={demoPage} setDemoPage={setDemoPage} />
+      ) : (
+        ""
+      )}
 
-      {demoPage === 5 ? <ReqQuestion5 demoPage={demoPage} setDemoPage={setDemoPage} /> : ""}
+      {demoPage === 5 ? (
+        <ReqQuestion5 demoPage={demoPage} setDemoPage={setDemoPage} />
+      ) : (
+        ""
+      )}
 
       {/* -----------------Optional Questions  ------------------------------------*/}
 
       {demoPage === 6 ? (
         <div>
-          <OptionalQuestionsInfo demoPage={demoPage} setDemoPage={setDemoPage} />
+          <OptionalQuestionsInfo
+            demoPage={demoPage}
+            setDemoPage={setDemoPage}
+          />
         </div>
       ) : (
         ""
@@ -67,7 +102,11 @@ export default function Reporting() {
         ""
       )}
 
-      {demoPage === 9 ? <YesContactInfo demoPage={demoPage} setDemoPage={setDemoPage} /> : ""}
+      {demoPage === 9 ? (
+        <YesContactInfo demoPage={demoPage} setDemoPage={setDemoPage} />
+      ) : (
+        ""
+      )}
 
       {demoPage === 10 ? (
         <FinalSubmission demoPage={demoPage} setDemoPage={setDemoPage} />
